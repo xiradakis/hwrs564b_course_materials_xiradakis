3cd8eb6d552a19c8eeaf429d028c23a70f1ea90f
Laura Condon <lecondon@email.arizona.edu>
2026-01-16 13:11:48 +0000

    fixing env name

--- postbuild.sh
+++ postbuild.sh
@@ -24,4 +24,4 @@ source ./.venv/bin/activate
 cd ..
 mkdir -p ./modflow
 python -c "from flopy.utils import get_modflow; get_modflow('./modflow')"
-python -m ipykernel install --user --name hwrs564a
+python -m ipykernel install --user --name hwrs564b
